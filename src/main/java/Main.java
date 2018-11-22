685635ff9845c35af45f83b5cac094c546d4c0e1
radekz0 <r.zielinski1221@gmail.com>
2018-11-22 16:00:02 +0100

    Displaying only certain parts of Json working.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -25,7 +25,7 @@ public class Main {
 //        System.out.println(writer);
         // do this
         get("template-example", (req, res) -> {
-            String templateVariable = "Jestę programistę";
+            String templateVariable = "Hello!";
             Map<String, Object> model = new HashMap<>();
             model.put("message",templateVariable);
 
